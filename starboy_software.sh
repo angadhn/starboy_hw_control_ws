995a34a57df94b998c49c4c805ee5c9b081309e6
angadh <angadh.n@gmail.com>
2018-03-08 17:09:39 +0000

    rm moveit pak

--- starboy_software.sh
+++ starboy_software.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+export ROS_IP=$(hostname -I)
+export ROS_HOSTNAME=$ROS_IP
+export ROS_MASTER_IP=192.168.0.11:11311
+export ROS_MASTER_URI=http://192.168.0.11:11311
+
 source devel/setup.bash
 roslaunch starboy_software.launch
