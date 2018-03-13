8d5d1984d0b85b618a4c9c4f49f43e2014060ceb
angadh <angadh.n@gmail.com>
2018-03-13 20:49:55 +0000

    visual servoing node

--- starboy_software.sh
+++ starboy_software.sh
@@ -5,4 +5,4 @@ export ROS_MASTER_IP=192.168.0.11:11311
 export ROS_MASTER_URI=http://192.168.0.11:11311
 
 source devel/setup.bash
-roslaunch starboy_software.launch
+roslaunch starboy_software.launch pipeline:=ompl
